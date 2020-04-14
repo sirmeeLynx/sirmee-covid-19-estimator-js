7373eb0ec3f27d4460556a713e947a74678c7dd7
sirmeeLynx <blessedsamuelodu@gmail.com>
2020-04-14 15:03:34 +0100

    Completed challenge one

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+const estimateInfectionSpread = (reportedCases, infectionMultiplierFactor) => {
+    //challenge 1
+    let currentlyInfected, infectionsByRequestedTime;
+    currentlyInfected = reportedCases * 10;
+    infectionsByRequestedTime = currentlyInfected * (1024); // 2^(30 % 3)
+    return {currentlyInfected, infectionsByRequestedTime};
+}
+
+const covid19ImpactEstimator = (data) => {
+    let { reportedCases } = data;
+    let impact, severeImpact;
+    //best case estimation
+    impact = estimateInfectionSpread(reportedCases, 10);
+    //severe case estimation
+    severeImpact = estimateInfectionSpread(reportedCases, 50);
+    return {
+        data: data, 
+        impact: impact,  
+        severeImpact: severeImpact 
+    }
+};
 
 export default covid19ImpactEstimator;
