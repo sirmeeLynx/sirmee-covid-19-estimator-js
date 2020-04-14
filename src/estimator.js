e5c33578c7a95f35b8543ccc89bd39638efeefb5
sirmeeLynx <blessedsamuelodu@gmail.com>
2020-04-14 15:23:18 +0100

    Fix eslint code style issues

--- src/estimator.js
+++ src/estimator.js
@@ -1,23 +1,22 @@
 const estimateInfectionSpread = (reportedCases, infectionMultiplierFactor) => {
-    //challenge 1
-    let currentlyInfected, infectionsByRequestedTime;
-    currentlyInfected = reportedCases * 10;
-    infectionsByRequestedTime = currentlyInfected * (1024); // 2^(30 % 3)
-    return {currentlyInfected, infectionsByRequestedTime};
-}
+  // challenge 1
+  const currentlyInfected = reportedCases * infectionMultiplierFactor;
+  const infectionsByRequestedTime = currentlyInfected * (1024); // 2^(30 % 3)
+  return { currentlyInfected, infectionsByRequestedTime };
+};
 
 const covid19ImpactEstimator = (data) => {
-    let { reportedCases } = data;
-    let impact, severeImpact;
-    //best case estimation
-    impact = estimateInfectionSpread(reportedCases, 10);
-    //severe case estimation
-    severeImpact = estimateInfectionSpread(reportedCases, 50);
-    return {
-        data: data, 
-        impact: impact,  
-        severeImpact: severeImpact 
-    }
+  const { reportedCases } = data;
+  // let impact, severeImpact;
+  // best case estimation
+  const impact = estimateInfectionSpread(reportedCases, 10);
+  // severe case estimation
+  const severeImpact = estimateInfectionSpread(reportedCases, 50);
+  return {
+    data,
+    impact,
+    severeImpact
+  };
 };
 
 export default covid19ImpactEstimator;
